1ad3b9c60f3981cb55869b20c22a2562d1844022
Jonas <jonas.eukan@gmail.com>
2019-12-03 11:59:08 +0800

    Change path

--- db-script.sh
+++ db-script.sh
@@ -4,7 +4,7 @@ mkdir -p "./data/app"
 mkdir -p "./data/db"
 docker network create docker-net
 docker pull postgres
-docker run --rm --name pg-docker --network docker-net -e POSTGRES_PASSWORD=docker -e POSTGRES_DB=harvester_api -d postgres -v ./data/db:/var/lib/postgresql/data postgres
+docker run --rm --name pg-docker --network docker-net -e POSTGRES_PASSWORD=docker -e POSTGRES_DB=harvester_api -d -v $PWD/data/db:/var/lib/postgresql/data postgres
 docker build ./harvester_api -t harvester_api
-docker run --rm  --name harvester_api --network docker-net  -v ./data/api:/app/downloads -d harvester_api
+docker run --rm  --name harvester_api --network docker-net  -v $PWD/data/api:/app/downloads -d harvester_api
 docker build ./harvestingapp -t harvester_api
